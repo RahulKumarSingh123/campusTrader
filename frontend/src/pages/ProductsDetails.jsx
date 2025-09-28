30ab45319d6c6374c702a0e90a783e681c2db1fe
RahulKumarSingh123 <rahulkumarsingh9389@gmail.com>
2025-09-28 12:48:11 +0530

    fiexed lintiting issues

--- frontend/src/pages/ProductsDetails.jsx
+++ frontend/src/pages/ProductsDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams,useNavigate } from "react-router-dom"
+import { useParams} from "react-router-dom"
 import axios from "axios";
 export default function ProductDetails(){
     // const [id,setId]=useState();
